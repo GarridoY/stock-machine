bbccc9646cfc82ae94e515199470e7c627d4fef5
GarridoY <dgmn17@student.aau.dk>
2023-08-28 22:45:26 +0200

    Init

---
+++ src/app/api/stocks/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server'
+ 
+export async function GET() {
+  const data = {
+    "name": 'Stonks',
+    "prices": [1.2, 1.9, 1.3, 2.0, 1.4]
+  }
+ 
+  return NextResponse.json({ data })
+}
