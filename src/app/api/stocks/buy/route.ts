544da15f16cc326ff781205352d76a068f9d07ce
Daniél Garrido <danielgarrido7@hotmail.com>
2023-09-21 17:04:05 +0200

    Added query params to stock buy endpoint

--- src/app/api/stocks/buy/route.ts
+++ src/app/api/stocks/buy/route.ts
@@ -1,7 +1,15 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 import { buyStock } from '@/logic/stocks';
 
 export async function POST(request: Request) {
-	const success = buyStock(1);
+	const url = new URL(request.url);
+	const params = url.searchParams;
+
+	const stockId = params.get("id");
+	if (stockId == null) {
+		return NextResponse.json(false);
+	}
+
+	const success = buyStock(parseInt(stockId));
 	return NextResponse.json(success);
 }
